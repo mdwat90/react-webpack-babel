1b801e15f904655277e34f556af863a0115201a6
David Watson <mwatson@gogoair.com>
2020-10-14 13:39:53 -0600

    initialize Firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
+import * as firebase from 'firebase';
 import './App.css';
 
 const App: React.FC = (): any => {
+  var firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DB_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID,
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  const signUp = () => {
+    firebase.database().ref('users/').set({
+      username: 'David',
+      email: 'test@test.com',
+    });
+  };
   return (
     <div>
-      <h1>YAY! It's working!</h1>
+      <h1>Welcome</h1>
+      <button onClick={signUp}>Sign Up</button>
     </div>
   );
 };
