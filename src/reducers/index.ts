9bcb1fe99e9ec9866cdfe222d549d58c0bdb36d9
David Watson <mdwat90@gmail.com>
2020-10-29 23:07:59 -0600

    rename bulletin_reducer

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import authReducer from './auth_reducer';
-import mainReducer from './bulletin_reducer';
+import mainReducer from './main_reducer';
 
 export default combineReducers({
   authReducer,
